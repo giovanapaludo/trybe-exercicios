30d8793bd739d4dc91be0f99003756623f1c5805
Giovana Paludo <giovanacpaludo@gmail.com>
2022-08-18 17:22:33 -0300

    Adiciona exercício 3

---
+++ 1-fundamentos/4_bloco/1_dia/3-exercicios/script.js
@@ -0,0 +1,13 @@
+const nota = 55;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!");
+}
+
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na lista de espera");
+}
+
+else {
+    console.log("Você foi reprovado");
+}
