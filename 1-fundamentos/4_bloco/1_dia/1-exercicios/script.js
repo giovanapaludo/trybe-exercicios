60f63451a1c27f2f1572318421da39193f917300
Giovana Paludo <giovanacpaludo@gmail.com>
2022-08-18 16:11:29 -0300

    Adiciona const e let

---
+++ 1-fundamentos/4_bloco/1_dia/1-exercicios/script.js
@@ -0,0 +1,9 @@
+const myName = "Giovana";
+const birthCity = "Porto Alegre";
+let birthYear = 1998;
+birthYear = 2030;
+birthCity = "RJ";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
