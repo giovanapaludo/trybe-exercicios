e440f2b850ad1cff6d48c1fc52d2395998ad2572
Giovana Paludo <giovanacpaludo@gmail.com>
2022-08-19 12:37:24 -0300

    Adiciona exercicio 3

---
+++ 1-fundamentos/4_bloco/1_dia/exercicios/script3.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 7;
+const c = 9;
+
+if (a > b && a > c) {
+    console.log(a + " é maior que " + b + " e " + c);
+}
+
+else if (b > a && b > c) {
+    console.log(b + " é maior que " + a + " e " + c);
+}
+
+else (c > a && c > b) 
+{ 
+    console.log(c + " é maior que " + a + " e " + b);
+}
