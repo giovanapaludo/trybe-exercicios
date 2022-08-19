d5045281b675d5304faa50f6fc3b02d3c618e00a
Giovana Paludo <giovanacpaludo@gmail.com>
2022-08-19 12:23:45 -0300

    Adiciona exercicio 2

---
+++ 1-fundamentos/4_bloco/1_dia/exercicios/script2.js
@@ -0,0 +1,14 @@
+const a = 20;
+const b = 30;
+
+if (a > b) {
+    console.log(a + " é maior que " + b); 
+}
+
+else if (b >a) {
+    console.log(b + " é maior que " + a);
+}
+
+else {
+    console.log("os números são iguais");
+}
