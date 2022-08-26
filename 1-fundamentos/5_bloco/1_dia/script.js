71c0084fb65784cb8cb3e56895dee69268be7410
Giovana Paludo <giovanacpaludo@gmail.com>
2022-08-26 17:20:58 -0300

    Adiciona

---
+++ 1-fundamentos/5_bloco/1_dia/script.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Orgulho e Preconceito";
+document.querySelector("#second-paragraph").innerText = "Enemies-to-lovers, pining, oblivious Elizabeth";
+document.getElementById("subtitle").innerText = "Jane Austen";
+document.querySelectorAll(".parag")[0].style.color = 'red';
+document.querySelector("h4").style.color = 'blue';
---
+++ 1-fundamentos/5_bloco/1_dia/script2.js
@@ -0,0 +1,5 @@
+document.getElementById("header-container").style.background = 'green';
+document.querySelector("section").style.background = 'pink';
+document.querySelectorAll("h3").style.background = 'purple';
+document.querySelectorAll("section h3").style.background = 'black';
+document.getElementsByClassName("no-emergency-tasks").style.background = 'black';
